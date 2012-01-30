e8e59ab2b7ca736f2d6c31e4144549adb78665d6
Erwan Ameil <idlewan@gmail.com>
2012-01-30 09:01:39 +0100

    Highlight letters as they are typed

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -1,3 +1,77 @@
+var keyObjects = [];
+var special_keys = {
+    17: "ctrl",
+    18: "alt",
+    91: "meta", // left
+    92: "meta", // right
+    32: "space",
+
+    27: "esc",
+    9 : "tab",
+    20: "caps",
+    16: "shift",
+    8 : "backspace",
+    13: "return",
+
+    /*
+    36: "home",
+    35: "end",
+    33: "pageup",
+    34: "pagedown",
+    46: "delete",
+
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    */
+
+    188: "<",
+    192: "`",
+    49: "1",
+    50: "2",
+    51: "3",
+    52: "4",
+    53: "5",
+    54: "6",
+    55: "7",
+    56: "8",
+    57: "9",
+    48: "0",
+    189: "-",
+    187: "=",
+
+    219: "[",
+    221: "]",
+
+    186: ";",
+    222: "'",
+    220: "\\",
+
+    188: ",",
+    190: ".",
+    191: "/",
+}
+
+function initKeyboard()
+{
+     var keyboardUl = document.getElementById("keyboard");
+     for ( var index=0; index < keyboardUl.children.length; index++ )
+     {
+         keyObjects[keyboardUl.children[index].textContent] = keyboardUl.children[index];
+     }
+}
+function toggleShift()
+{
+    $('.letter').toggleClass('uppercase');
+    $('.symbol span').toggle();
+}
+function highlightKey(keyText)
+{
+    $(keyObjects[keyText]).addClass("highlighted");
+    $(keyObjects[keyText]).removeClass("easeout");
+}
+
 
 function removeLetterFromTarget() {
     activeTarget.word = activeTarget.word.substring(1);
@@ -21,7 +95,7 @@ function removeLetterFromTarget() {
 function recognizeWord(e){
     var unicode = e.keyCode ? e.keyCode : e.charCode;
     var actualkey = String.fromCharCode(unicode).toLowerCase();
-//    console.log(actualkey);
+    highlightKey(actualkey);
 
     if (activeTarget == null){
         for (i=0; i < enemies.length; i++) {
@@ -44,4 +118,17 @@ function recognizeWord(e){
     }
 //    console.log(activeTarget);
 }
+function unhighlight(e)
+{
+    var unicode = e.keyCode ? e.keyCode : e.charCode;
+    var actualkey = String.fromCharCode(unicode).toLowerCase();
+    if (actualkey)
+    {
+        $(keyObjects[actualkey]).removeClass("highlighted");
+        $(keyObjects[actualkey]).addClass("easeout");
+    }
+}
 document.onkeydown=recognizeWord;
+document.onkeyup=unhighlight;
+//$(document).bind('keydown', 'shift', toggleShift);
+//$(document).bind('keyup', 'shift', toggleShift);
